1fb28d57054198a47527903e5bf4dcf6e14d2013
Picasso <urvishp13@gmail.com>
2023-11-08 15:53:07 -0500

    wrote ENDORSEMENTS to DOM using Firebase and set styles for them

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// javascript
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js'
+import { getDatabase, 
+         ref, 
+         push,
+         onValue
+} from 'https://www.gstatic.com/firebasejs/10.5.2/firebase-database.js'
+
+const endorsementInputEl = document.getElementById('endorsement-input')
+const publishBtn = document.getElementById('publish-btn')
+const endorsementsSection = document.getElementById('endorsements')
+
+const firebaseConfig = {
+    databaseURL: 'https://we-are-the-champions-d748d-default-rtdb.firebaseio.com/'
+}
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+const endorsementsDB = ref(database, "endorsements")
+
+// when the database resets
+
+publishBtn.addEventListener('click', function() {
+    // extract the endorsement from the textarea
+    const endorsement = endorsementInputEl.value
+
+    // push it to the database
+    push(endorsementsDB, endorsement)
+
+    console.log('endorsement pushed')
+
+    // add it to the endorsements section
+    appendEndorsementToEndorsementsSection(endorsement)
+
+    // clear it from the textarea
+    clearEndorsementInputEl()
+})
+
+function appendEndorsementToEndorsementsSection(endorsementText) {
+    endorsementsSection.innerHTML += `
+        <div class="endorsement-wrapper">
+            <p class="endorsement">${endorsementText}</p>
+        </div>
+    `
+}
+
+function clearEndorsementInputEl() {
+    endorsementInputEl.value = ''
+}
